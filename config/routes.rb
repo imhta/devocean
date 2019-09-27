394ad3d6c1141da7a35a6f97e00e18cf45bf5c1d
imhta <imhtapm@gmail.com>
2019-09-27 20:49:58 +0530

    cleaning route config file, removing unneeded comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get 'sign_up', to: 'users/registrations#new'
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
 end
