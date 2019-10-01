e1b2995c843a44d309eca199102412fd4970fcf2
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-01 20:46:44 +0500

    generated devise views and users_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  before_action :authenticate_user!
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
