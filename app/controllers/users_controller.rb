e783ba0de8c66ff5efb8f3fe8e2651e35ee2aab7
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-03 17:58:59 +0500

    edited user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
-    @user =User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
-    @users=User.all()
+    @users= User.all()
   end
 end
