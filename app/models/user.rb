42e665192be3854c1a79e3b442c47afcb8cb23f7
imhta <imhtapm@gmail.com>
2019-10-03 19:50:50 +0530

    association has_many added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
