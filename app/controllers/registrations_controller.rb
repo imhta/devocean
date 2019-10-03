20a066751fdfed77da1ef52b65185f9af03dafd2
imhta <imhtapm@gmail.com>
2019-10-03 19:38:38 +0530

    long line auto fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
-    params.require(:user).permit(:email, :first_name, :last_name, :dob, :gender, :img_link, :password, :password_confirmation)
+    params.require(:user).permit(:email, :first_name, :last_name, :dob, :gender,
+                                 :img_link, :password, :password_confirmation)
   end
 end
