77d7ab335e5215a04518ff156fe0ac59eed1237e
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-03 17:58:31 +0500

    generatere registrations controller add signup params

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,5 @@
+class RegistrationsController < Devise::RegistrationsController
+  def sign_up_params
+    params.require(:user).permit(:email, :first_name, :last_name, :dob, :gender, :img_link, :password, :password_confirmation)
+  end
+end
