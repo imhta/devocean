35a2d61f44c757f5c399e3d417f791be13226ee3
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-04 15:34:42 +0500

    added associations for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-
+  has_many :comments
   validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_many :posts
+  has_may :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
