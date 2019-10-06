8658480a736d39c2e8aaa042a7b927f83e796c42
imhta <imhtapm@gmail.com>
2019-10-06 20:37:14 +0530

    chnaged redirect route to home_index_path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def after_sign_up_path_for(resource)
-    stored_location_for(resource) || users_index_path
+    stored_location_for(resource) || home_index_path
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,6 @@ class SessionsController < Devise::SessionsController
   private
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || users_index_path
+    stored_location_for(resource) || home_index_path
   end
 end
