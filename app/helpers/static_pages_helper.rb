d647b450e7daa5f51a5c13f5abd6c7ffa45338e4
imhta <imhtapm@gmail.com>
2019-10-06 14:24:47 +0530

    added home static page and static page controlller

---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StaticPagesHelper
+  def home; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: "registrations"}
 
   get 'users/show'
-  root 'users#index'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
