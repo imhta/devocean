493c6098fef1b7e3f4e37ab7cd18d633db52a474
imhta <imhtapm@gmail.com>
2019-10-07 22:27:33 +0530

    basic post feature completed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @post = Post.new
+    @posts = current_user.posts.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: "registrations", sessions: "sessions"}
 
   get 'users/index'
+  post 'post', to: 'posts#create' 
   authenticated :user do
     root to: 'home#index', as: :authenticated_root
   end
   root 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
