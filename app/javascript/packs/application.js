491ff7bfc5aa9a16fba1a0f688bba8f76d640498
imhta <imhtapm@gmail.com>
2019-10-08 15:09:11 +0530

    pwa ready and configged

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,12 @@ import '../stylesheets/application'
 
 require("trix")
 require("@rails/actiontext")
+
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+      .then(function(reg) {
+        console.log('[Companion]', 'Service worker registered!');
+        console.log(reg);
+      });
+  }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  # pwa routes
+  get '/service-worker.js' => "service_worker#service_worker"
+  get '/manifest.json' => "service_worker#manifest"
   devise_for :users, :controllers => {registrations: "registrations", sessions: "sessions"}
 
   get 'users/index'
