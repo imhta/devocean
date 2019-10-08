792b168b0d75284063232567258b6a6311dc4ca0
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-08 15:29:29 +0500

    added  actions for likes contrller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,35 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create 
+    if user_signed_in?
+      @like=Like.new(like_params)
+      @like.save
+      flash[:notice]="Post liked"
+      # flash[:notice]="#{@post.title} succesfully liked"
+      # redirect
+    end
+  end
+
+  def destroy
+    @like.destroy if like_owner? && user_signed_in?
+    # redirect
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:post_id , :user_id)
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def like_owner?
+    current_user = @like.user
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
+  end
 end
