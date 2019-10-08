1788b17f8a503aa621ffd6059120ee3395999e75
imhta <imhtapm@gmail.com>
2019-10-08 17:11:59 +0530

    added dependent destory and validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :dob, presence: true
+  validates :gender, presence: true
 
   def display_name
     first_name + ' ' + last_name
