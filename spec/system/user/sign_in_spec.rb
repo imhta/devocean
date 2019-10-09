e32d1e98e7a00d53c7a7241088cf46e39c0856f3
imhta <imhtapm@gmail.com>
2019-10-09 19:44:50 +0530

    added system test (intregation test)

---
+++ spec/system/user/sign_in_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'User Sign In', type: :system do
+  context 'User Sign In' do
+    let(:user) { FactoryBot.create(:user) }
+    before(:each) do
+      visit root_path
+      within 'form' do
+        fill_in 'Email', with: user.email
+      end
+    end
+
+    scenario 'User signs in successfully' do
+      within 'form' do
+        fill_in 'Password', with: user.password
+        click_button 'Log in'
+      end
+      expect(page).to have_content('Signed in successfully.')
+    end
+
+    scenario 'User fills in incorrect password' do
+      within 'form' do
+        fill_in 'Password', with: 'wrong password'
+        click_button 'Log in'
+      end
+      expect(page).to have_content('Invalid Email or password.')
+    end
+  end
+end
---
+++ spec/system/user/sign_up_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'User page', type: :system do
+  context 'User Sign Up' do
+    before(:each) do
+      visit root_path
+      click_link 'Sign up'
+      within 'form' do
+        fill_in 'First name', with: 'First'
+        fill_in 'Last name', with: 'Last'
+        fill_in 'Email', with: 'test@example.com'
+        fill_in 'Gender', with: 'Men',
+        fill_in 'Dob', with: '12/11/1997',
+        fill_in 'Password', with: 'test123'
+        fill_in 'Password confirmation', with: 'test123'
+      end
+    end
+
+    scenario 'User signs up successfully' do
+      click_button 'Sign up'
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+
+    scenario 'User fills in incorrect information' do
+      within 'form' do
+        fill_in 'First name', with: ''
+        click_button 'Sign up'
+      end
+      expect(page).to have_content("First name can't be blank")
+    end
+  end
+end
