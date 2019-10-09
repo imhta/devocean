53faad32f058e044837ea7cf99ed52e60eca6efa
imhta <imhtapm@gmail.com>
2019-10-09 21:44:39 +0530

    removed unused controller spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,4 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def index
-    @users = User.all
-  end
 end
