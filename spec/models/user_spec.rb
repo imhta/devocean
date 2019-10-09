9f131c7b44b2c4dafef0d53f2b062e5b4b377b68
imhta <imhtapm@gmail.com>
2019-10-09 21:59:19 +0530

    model spec added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,23 @@
-# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:dob) }
+    it { is_expected.to validate_presence_of(:gender) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:posts) }
+  end
 end
