f176137a748da03092420524a519bd1011cf6122
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-14 19:21:16 +0500

    added methods for friendship in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
@@ -16,5 +19,25 @@ class User < ApplicationRecord
 
   def initial
     first_name[0].upcase
+  end
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  def friend_request
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def comfirm_friend(usere)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user }
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
   end
 end
