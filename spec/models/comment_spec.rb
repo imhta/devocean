2764a7a81b8ce3706e68074030504f8de4125690
imhta <imhtapm@gmail.com>
2019-10-14 16:23:19 +0530

    comment model spec added

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:body) }
+  end
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:post) }
+  end
 end
