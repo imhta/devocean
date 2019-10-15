b3e9af1304ae471eece6334fea99dc38fd56f3cf
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-10-15 17:07:14 +0500

    changed friendship and friend reques tables

--- db/migrate/20191014110446_create_friendships.rb
+++ db/migrate/20191014110446_create_friendships.rb
@@ -2,10 +2,11 @@ class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :friend, null: false, foreign_key: true
+      t.references :friend
       t.integer :status
 
       t.timestamps
     end
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
--- db/migrate/20191014154055_create_friend_requests.rb
+++ db/migrate/20191014154055_create_friend_requests.rb
@@ -2,9 +2,10 @@ class CreateFriendRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :friend_requests do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :friend, null: false, foreign_key: true
+      t.references :friend
 
       t.timestamps
     end
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
