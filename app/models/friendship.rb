808c64ed861298a9c77645c09e3ad1046d393124
imhta <imhtapm@gmail.com>
2019-10-15 12:04:14 +0530

    removed friendship model because merged mistakenly

--- app/models/friendship.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: 'User'
-end
--- spec/factories/friendships.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :friendship do
-    user { nil }
-    friend { nil }
-    status { 1 }
-  end
-end
