1d87ee06c95fa99c88765f99692196767e1af3b7
Pon Muthu Selvam N <imhtapm@gmail.com>
2019-10-15 11:55:51 +0530

    Revert "Comments and likes (with 1st review changes)"

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,14 +6,14 @@ class LikesController < ApplicationController
   def create
     return destroy if already_liked?
 
-    like = current_user.likes.build(like_params)
+    like = Like.new(like_params)
     like.save
     flash[:notice] = 'Post liked'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    like = current_user.likes.find_by(like_params)
+    like = Like.find_by(like_params)
     like.destroy
     redirect_back(fallback_location: root_path)
   end
@@ -21,10 +21,10 @@ class LikesController < ApplicationController
   private
 
   def like_params
-    params.require(:like).permit(:post_id)
+    params.require(:like).permit(:post_id, :user_id)
   end
 
   def already_liked?
-    current_user.likes.where(like_params).exists?
+    Like.where(like_params).exists?
   end
 end
