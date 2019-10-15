a729f2daeb1251fa3e92ae2d0c6625b366452c49
imhta <imhtapm@gmail.com>
2019-10-15 12:05:30 +0530

    removing friendship methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
-
   has_many :likes
-
-  has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend'
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -22,25 +18,5 @@ class User < ApplicationRecord
 
   def initial
     first_name[0].upcase
-  end
-
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-    friends_array.compact
-  end
-
-  def friend_request
-    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
-  end
-
-  def comfirm_friend(_usere)
-    friendship = inverse_friendships.find { |fs| fs.user == user }
-    friendship.confirmed = true
-    friendship.save
-  end
-
-  def friend?(user)
-    friends.include?(user)
   end
 end
