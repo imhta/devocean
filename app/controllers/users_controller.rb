a594f325d1258639618f1ec7d19fd30bbe68d00a
imhta <imhtapm@gmail.com>
2019-10-16 21:50:48 +0530

    added user filter, for current user and user friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.all - [current_user] - current_user.friends
   end
 
   def remove_friend(friend)
