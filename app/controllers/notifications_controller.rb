7c0b67863048ddeb0d1cb934543a6c792e7780a8
imhta <imhtapm@gmail.com>
2019-10-16 22:45:26 +0530

    allow only auth user to notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = current_user.requested_friends
   end
