535666a3b25dcdd20b030291d6b43091b870d69c
imhta <imhtapm@gmail.com>
2019-10-22 20:05:09 +0530

    added keys to devise config file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,10 +257,10 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   # ==> OmniAuth
-  # Add a new OmniAuth provider. Check the wiki for more information on setting
-  # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  FB_APP_ID = Rails.application.credentials.dig(:fb_app_id)
+  FB_SECRET_ID = Rails.application.credentials.dig(:fb_secret_id)
 
+  config.omniauth :facebook, FB_APP_ID, FB_SECRET_ID, token_params: { parse: :json }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
