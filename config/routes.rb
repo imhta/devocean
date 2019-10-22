f86c519cfb5e8621ccf6529fac30ff1ae4d33517
imhta <imhtapm@gmail.com>
2019-10-22 19:49:43 +0530

    adding privacy policy and config router

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # pwa routes
   get '/service-worker.js' => "service_worker#service_worker"
   get '/manifest.json' => "service_worker#manifest"
+  get '/privacy-policy' => 'static_pages#privacy'
   devise_for :users, :controllers => {registrations: "registrations"}
 
   get 'users/index'
